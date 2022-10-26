da50cd3704d5edd73e79a03a789d83bc7f9f5942
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-10-26 11:58:05 -0300

    Initial

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  state = {
+    name: 'Guilherme Marins',
+    counter: 0
+  };
+
+  handlePClick = () => {
+    this.setState({ name: 'Ferreira' });
+  }
+
+  handleAClick = (event) => {
+    event.preventDefault();
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  }
+
+  render() {
+    const { name, counter } = this.state;
+
+    return (
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={this.handlePClick}>
+          {name} {counter}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={this.handleAClick}
+        >
+          Este é o link
+        </a>
+      </header>
+    </div>
+    )
+  }
+}
+
+
+export default App;
