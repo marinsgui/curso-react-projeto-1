8814bb1b6abdb7ddcb1bf5f34ddf8381ab14be72
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-10-28 11:49:02 -0300

    Posts e fotos

--- src/App.js
+++ src/App.js
@@ -1,44 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
   state = {
-    name: 'Guilherme Marins',
-    counter: 0
+    posts: []
   };
 
-  handlePClick = () => {
-    this.setState({ name: 'Ferreira' });
+  componentDidMount() {
+    this.loadPosts();
   }
 
-  handleAClick = (event) => {
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1 });
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url }
+    })
+
+    this.setState({ posts: postsAndPhotos });
   }
+
 
   render() {
-    const { name, counter } = this.state;
+    const { posts } = this.state;
 
     return (
-      <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={this.handlePClick}>
-          {name} {counter}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          onClick={this.handleAClick}
-        >
-          Este é o link
-        </a>
-      </header>
-    </div>
+      <section className='container'>
+        <div className="posts">
+          {posts.map(post => (
+            <div className='post'>
+              <img src={post.cover} alt={post.title}/>
+              <div key={post.id} className='post-content'>
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     )
   }
 }
